fd7c7ab72a894471778a860463a1102b7e5521be
Blanca Rosales <rosales.taizx@gmail.com>
2022-05-19 21:41:28 -0600

    cargo changes and added the solution

--- Cargo/FrmCargo.vb
+++ Cargo/FrmCargo.vb
@@ -19,7 +19,18 @@
 
     End Sub
 
-    Private Sub TableLayoutPanel1_Paint(sender As Object, e As PaintEventArgs) Handles TableLayoutPanel1.Paint
+    Private Sub TableLayoutPanel1_Paint(sender As Object, e As PaintEventArgs)
+
+    End Sub
+
+    Private Sub BtnAddCar_Click(sender As Object, e As EventArgs) Handles BtnAddCar.Click
+        FrmAddCargo.MdiParent = Me.MdiParent
+        FrmAddCargo.Show()
+
+    End Sub
+
+    Private Sub BtnModCar_Click(sender As Object, e As EventArgs) Handles BtnModCar.Click
+        FrmModCargo.Show()
 
     End Sub
 End Class
