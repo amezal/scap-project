3702d45eaafbf9510229439db00ee267314ec6dc
Armando <amezal01@hotmail.com>
2022-05-21 17:52:25 -0600

    listar y mostrar empleados

--- Empleados/FrmEmpleados.designer.vb
+++ Empleados/FrmEmpleados.designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class FrmEmpleados
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,29 +20,28 @@ Partial Class FrmEmpleados
     'NOTE: The following procedure is required by the Windows Form Designer
     'It can be modified using the Windows Form Designer.  
     'Do not modify it using the code editor.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
         Me.ToolStrip1 = New System.Windows.Forms.ToolStrip()
         Me.ToolStripButton1 = New System.Windows.Forms.ToolStripButton()
         Me.ToolStripSeparator1 = New System.Windows.Forms.ToolStripSeparator()
         Me.ToolStripButton2 = New System.Windows.Forms.ToolStripButton()
         Me.ToolStripSeparator2 = New System.Windows.Forms.ToolStripSeparator()
+        Me.BtnMod = New System.Windows.Forms.ToolStripButton()
         Me.ToolStripSeparator3 = New System.Windows.Forms.ToolStripSeparator()
         Me.ToolStripButton4 = New System.Windows.Forms.ToolStripButton()
         Me.ToolStripTextBox1 = New System.Windows.Forms.ToolStripTextBox()
-        Me.CheckBox3 = New System.Windows.Forms.CheckBox()
-        Me.CheckBox2 = New System.Windows.Forms.CheckBox()
-        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
-        Me.ToolStripButton3 = New System.Windows.Forms.ToolStripButton()
+        Me.DgvEmpleados = New System.Windows.Forms.DataGridView()
         Me.ToolStrip1.SuspendLayout()
+        CType(Me.DgvEmpleados, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'ToolStrip1
         '
-        Me.ToolStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ToolStripButton1, Me.ToolStripSeparator1, Me.ToolStripButton2, Me.ToolStripSeparator2, Me.ToolStripButton3, Me.ToolStripSeparator3, Me.ToolStripButton4, Me.ToolStripTextBox1})
+        Me.ToolStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ToolStripButton1, Me.ToolStripSeparator1, Me.ToolStripButton2, Me.ToolStripSeparator2, Me.BtnMod, Me.ToolStripSeparator3, Me.ToolStripButton4, Me.ToolStripTextBox1})
         Me.ToolStrip1.Location = New System.Drawing.Point(0, 0)
         Me.ToolStrip1.Name = "ToolStrip1"
-        Me.ToolStrip1.Size = New System.Drawing.Size(434, 25)
+        Me.ToolStrip1.Size = New System.Drawing.Size(844, 25)
         Me.ToolStrip1.TabIndex = 0
         Me.ToolStrip1.Text = "ToolStrip1"
         '
@@ -74,6 +73,15 @@ Partial Class FrmEmpleados
         Me.ToolStripSeparator2.Name = "ToolStripSeparator2"
         Me.ToolStripSeparator2.Size = New System.Drawing.Size(6, 25)
         '
+        'BtnMod
+        '
+        Me.BtnMod.Image = Global.SCAP_LMBA_Tech.My.Resources.Resources.icons8_edit_production_order_32
+        Me.BtnMod.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.BtnMod.Name = "BtnMod"
+        Me.BtnMod.Size = New System.Drawing.Size(78, 22)
+        Me.BtnMod.Text = "Modificar"
+        Me.BtnMod.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage
+        '
         'ToolStripSeparator3
         '
         Me.ToolStripSeparator3.Name = "ToolStripSeparator3"
@@ -90,58 +98,34 @@ Partial Class FrmEmpleados
         '
         'ToolStripTextBox1
         '
+        Me.ToolStripTextBox1.Font = New System.Drawing.Font("Segoe UI", 9.0!)
         Me.ToolStripTextBox1.Name = "ToolStripTextBox1"
-        Me.ToolStripTextBox1.Size = New System.Drawing.Size(100, 25)
+        Me.ToolStripTextBox1.Size = New System.Drawing.Size(86, 25)
         '
-        'CheckBox3
+        'DgvEmpleados
         '
-        Me.CheckBox3.AutoSize = True
-        Me.CheckBox3.Location = New System.Drawing.Point(85, 220)
-        Me.CheckBox3.Name = "CheckBox3"
-        Me.CheckBox3.Size = New System.Drawing.Size(15, 14)
-        Me.CheckBox3.TabIndex = 8
-        Me.CheckBox3.UseVisualStyleBackColor = True
-        '
-        'CheckBox2
-        '
-        Me.CheckBox2.AutoSize = True
-        Me.CheckBox2.Location = New System.Drawing.Point(85, 189)
-        Me.CheckBox2.Name = "CheckBox2"
-        Me.CheckBox2.Size = New System.Drawing.Size(15, 14)
-        Me.CheckBox2.TabIndex = 7
-        Me.CheckBox2.UseVisualStyleBackColor = True
-        '
-        'CheckBox1
-        '
-        Me.CheckBox1.AutoSize = True
-        Me.CheckBox1.Location = New System.Drawing.Point(85, 156)
-        Me.CheckBox1.Name = "CheckBox1"
-        Me.CheckBox1.Size = New System.Drawing.Size(15, 14)
-        Me.CheckBox1.TabIndex = 6
-        Me.CheckBox1.UseVisualStyleBackColor = True
-        '
-        'ToolStripButton3
-        '
-        Me.ToolStripButton3.Image = Global.SCAP_LMBA_Tech.My.Resources.Resources.icons8_edit_production_order_32
-        Me.ToolStripButton3.ImageTransparentColor = System.Drawing.Color.Magenta
-        Me.ToolStripButton3.Name = "ToolStripButton3"
-        Me.ToolStripButton3.Size = New System.Drawing.Size(78, 22)
-        Me.ToolStripButton3.Text = "Modificar"
-        Me.ToolStripButton3.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage
+        Me.DgvEmpleados.AllowUserToAddRows = False
+        Me.DgvEmpleados.AllowUserToDeleteRows = False
+        Me.DgvEmpleados.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DgvEmpleados.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.DgvEmpleados.Location = New System.Drawing.Point(0, 25)
+        Me.DgvEmpleados.Name = "DgvEmpleados"
+        Me.DgvEmpleados.ReadOnly = True
+        Me.DgvEmpleados.Size = New System.Drawing.Size(844, 439)
+        Me.DgvEmpleados.TabIndex = 1
         '
         'FrmEmpleados
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(434, 321)
-        Me.Controls.Add(Me.CheckBox3)
-        Me.Controls.Add(Me.CheckBox2)
-        Me.Controls.Add(Me.CheckBox1)
+        Me.ClientSize = New System.Drawing.Size(844, 464)
+        Me.Controls.Add(Me.DgvEmpleados)
         Me.Controls.Add(Me.ToolStrip1)
         Me.Name = "FrmEmpleados"
         Me.Text = "FrmEmpleados"
         Me.ToolStrip1.ResumeLayout(False)
         Me.ToolStrip1.PerformLayout()
+        CType(Me.DgvEmpleados, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -155,8 +139,6 @@ Partial Class FrmEmpleados
     Friend WithEvents ToolStripSeparator3 As ToolStripSeparator
     Friend WithEvents ToolStripButton4 As ToolStripButton
     Friend WithEvents ToolStripTextBox1 As ToolStripTextBox
-    Friend WithEvents CheckBox3 As CheckBox
-    Friend WithEvents CheckBox2 As CheckBox
-    Friend WithEvents CheckBox1 As CheckBox
-    Friend WithEvents ToolStripButton3 As ToolStripButton
+    Friend WithEvents BtnMod As ToolStripButton
+    Friend WithEvents DgvEmpleados As DataGridView
 End Class
--- Empleados/FrmEmpleados.vb
+++ Empleados/FrmEmpleados.vb
@@ -1,9 +1,27 @@
 ﻿Public Class FrmEmpleados
-    Private Sub ToolStripLabel1_Click(sender As Object, e As EventArgs)
 
+    Dim empleados As New LMBADataSetTableAdapters.EmpleadosDgvTableAdapter
+    Dim idEmp As New Int32
+
+    Private Sub FrmEmpleados_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        llenarGrid()
     End Sub
 
-    Private Sub ToolStripButton2_Click(sender As Object, e As EventArgs) Handles ToolStripButton2.Click
+    Sub llenarGrid()
+        DgvEmpleados.DataSource = empleados.GetData()
+        DgvEmpleados.Columns("idDepartamento").Visible = False
+        DgvEmpleados.Refresh()
+    End Sub
 
+    Private Sub DgvEmpleados_DoubleClick(sender As Object, e As EventArgs) Handles DgvEmpleados.DoubleClick
+        Dim row = DgvEmpleados.CurrentRow.Index
+        Dim emp = DgvEmpleados.Rows(row)
+        Dim id = emp.Cells(0).Value
+        idEmp = id
+    End Sub
+
+    Private Sub BtnMod_Click(sender As Object, e As EventArgs) Handles BtnMod.Click
+        FrmModEmp.idEmp = idEmp
+        FrmModEmp.Show()
     End Sub
 End Class
