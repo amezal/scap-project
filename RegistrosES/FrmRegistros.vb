fd2831c601b9d9c85aff10a3ab826ec20a1445b9
Armando <amezal01@hotmail.com>
2022-05-22 15:47:31 -0600

    buscar registros empleado

--- RegistrosES/FrmRegistros.vb
+++ RegistrosES/FrmRegistros.vb
@@ -1,18 +1,24 @@
 ﻿Public Class FrmRegistros
-    Private Sub WinrSviewer1_Load(sender As Object, e As EventArgs)
 
-    End Sub
+    Dim empleado As New LMBADataSetTableAdapters.EmpleadosDgvTableAdapter
 
     Private Sub FrmRegistros_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-
-
+        llenarCbx()
     End Sub
 
-    Private Sub ReportViewer1_Load(sender As Object, e As EventArgs)
-
+    Private Sub llenarCbx()
+        Dim emp = empleado.GetData()
+        emp.Columns.Add("display", GetType(String), " Nombres + ' ' + Apellidos + ' - ' + ID")
+        CbxBuscar.DataSource = emp
+        CbxBuscar.DisplayMember = "display"
+        CbxBuscar.ValueMember = "ID"
     End Sub
 
-    Private Sub DataGridView1_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) 
-
+    Private Sub CbxBuscar_SelectedIndexChanged(sender As Object, e As EventArgs) Handles CbxBuscar.SelectedIndexChanged
+        Dim emp = empleado.GetData().Item(CbxBuscar.SelectedIndex)
+        TxtID.Text = "ID: " & emp.ID
+        TxtEmpleado.Text = "Nombres: " & emp.Nombres & " " & emp.Apellidos
+        TxtCargo.Text = "Cargo: " & emp.Cargo
+        TxtDpto.Text = "Departamento: " & emp.Departamento
     End Sub
 End Class
