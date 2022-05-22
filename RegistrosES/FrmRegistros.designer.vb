e92044b90b8b01fefab633ebf7171c09462f2e10
Armando <amezal01@hotmail.com>
2022-05-22 15:47:31 -0600

    buscar registros empleado

--- RegistrosES/FrmRegistros.designer.vb
+++ RegistrosES/FrmRegistros.designer.vb
@@ -22,119 +22,136 @@ Partial Class FrmRegistros
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        Me.components = New System.ComponentModel.Container()
         Me.ToolStrip1 = New System.Windows.Forms.ToolStrip()
-        Me.ToolStripButton1 = New System.Windows.Forms.ToolStripButton()
+        Me.BtnJustificar = New System.Windows.Forms.ToolStripButton()
         Me.ToolStripSeparator3 = New System.Windows.Forms.ToolStripSeparator()
-        Me.ToolStripButton4 = New System.Windows.Forms.ToolStripButton()
-        Me.ToolStripTextBox1 = New System.Windows.Forms.ToolStripTextBox()
-        Me.Label1 = New System.Windows.Forms.Label()
-        Me.Label2 = New System.Windows.Forms.Label()
-        Me.Label3 = New System.Windows.Forms.Label()
-        Me.RegistroBindingSource = New System.Windows.Forms.BindingSource(Me.components)
-        Me.DataSet1BindingSource = New System.Windows.Forms.BindingSource(Me.components)
+        Me.BtnBuscar = New System.Windows.Forms.ToolStripButton()
+        Me.CbxBuscar = New System.Windows.Forms.ComboBox()
+        Me.TxtID = New System.Windows.Forms.Label()
+        Me.TxtEmpleado = New System.Windows.Forms.Label()
+        Me.TxtCargo = New System.Windows.Forms.Label()
+        Me.TxtDpto = New System.Windows.Forms.Label()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
         Me.ToolStrip1.SuspendLayout()
-        CType(Me.RegistroBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
-        CType(Me.DataSet1BindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'ToolStrip1
         '
-        Me.ToolStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ToolStripButton1, Me.ToolStripSeparator3, Me.ToolStripButton4, Me.ToolStripTextBox1})
+        Me.ToolStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.BtnJustificar, Me.ToolStripSeparator3, Me.BtnBuscar})
         Me.ToolStrip1.Location = New System.Drawing.Point(0, 0)
         Me.ToolStrip1.Name = "ToolStrip1"
         Me.ToolStrip1.Size = New System.Drawing.Size(686, 25)
         Me.ToolStrip1.TabIndex = 1
         Me.ToolStrip1.Text = "ToolStrip1"
         '
-        'ToolStripButton1
+        'BtnJustificar
         '
-        Me.ToolStripButton1.Image = Global.SCAP_LMBA_Tech.My.Resources.Resources.icons8_new_window_30
-        Me.ToolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta
-        Me.ToolStripButton1.Name = "ToolStripButton1"
-        Me.ToolStripButton1.Size = New System.Drawing.Size(73, 22)
-        Me.ToolStripButton1.Text = "Justificar"
-        Me.ToolStripButton1.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage
+        Me.BtnJustificar.Image = Global.SCAP_LMBA_Tech.My.Resources.Resources.icons8_new_window_30
+        Me.BtnJustificar.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.BtnJustificar.Name = "BtnJustificar"
+        Me.BtnJustificar.Size = New System.Drawing.Size(73, 22)
+        Me.BtnJustificar.Text = "Justificar"
+        Me.BtnJustificar.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage
         '
         'ToolStripSeparator3
         '
         Me.ToolStripSeparator3.Name = "ToolStripSeparator3"
         Me.ToolStripSeparator3.Size = New System.Drawing.Size(6, 25)
         '
-        'ToolStripButton4
+        'BtnBuscar
         '
-        Me.ToolStripButton4.Image = Global.SCAP_LMBA_Tech.My.Resources.Resources.icons8_search_24
-        Me.ToolStripButton4.ImageTransparentColor = System.Drawing.Color.Magenta
-        Me.ToolStripButton4.Name = "ToolStripButton4"
-        Me.ToolStripButton4.Size = New System.Drawing.Size(62, 22)
-        Me.ToolStripButton4.Text = "Buscar"
-        Me.ToolStripButton4.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage
+        Me.BtnBuscar.Image = Global.SCAP_LMBA_Tech.My.Resources.Resources.icons8_search_24
+        Me.BtnBuscar.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.BtnBuscar.Name = "BtnBuscar"
+        Me.BtnBuscar.Size = New System.Drawing.Size(62, 22)
+        Me.BtnBuscar.Text = "Buscar"
+        Me.BtnBuscar.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage
         '
-        'ToolStripTextBox1
+        'CbxBuscar
         '
-        Me.ToolStripTextBox1.Name = "ToolStripTextBox1"
-        Me.ToolStripTextBox1.Size = New System.Drawing.Size(86, 25)
+        Me.CbxBuscar.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend
+        Me.CbxBuscar.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.ListItems
+        Me.CbxBuscar.FormattingEnabled = True
+        Me.CbxBuscar.Location = New System.Drawing.Point(157, 2)
+        Me.CbxBuscar.Name = "CbxBuscar"
+        Me.CbxBuscar.Size = New System.Drawing.Size(299, 21)
+        Me.CbxBuscar.TabIndex = 5
         '
-        'Label1
+        'TxtID
         '
-        Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(22, 38)
-        Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(187, 13)
-        Me.Label1.TabIndex = 2
-        Me.Label1.Text = "001 - Armando Alexander Meza López"
+        Me.TxtID.AutoSize = True
+        Me.TxtID.Location = New System.Drawing.Point(12, 31)
+        Me.TxtID.Name = "TxtID"
+        Me.TxtID.Size = New System.Drawing.Size(36, 13)
+        Me.TxtID.TabIndex = 6
+        Me.TxtID.Text = "ID: 88"
         '
-        'Label2
+        'TxtEmpleado
         '
-        Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(276, 38)
-        Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(61, 13)
-        Me.Label2.TabIndex = 3
-        Me.Label2.Text = "Cargo: Jefe"
+        Me.TxtEmpleado.AutoSize = True
+        Me.TxtEmpleado.Location = New System.Drawing.Point(72, 31)
+        Me.TxtEmpleado.Name = "TxtEmpleado"
+        Me.TxtEmpleado.Size = New System.Drawing.Size(213, 13)
+        Me.TxtEmpleado.TabIndex = 7
+        Me.TxtEmpleado.Text = "Empleado: Armando Alexander Meza Lopez"
         '
-        'Label3
+        'TxtCargo
         '
-        Me.Label3.AutoSize = True
-        Me.Label3.Location = New System.Drawing.Point(458, 38)
-        Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(125, 13)
-        Me.Label3.TabIndex = 4
-        Me.Label3.Text = "Departamento: Managua"
+        Me.TxtCargo.AutoSize = True
+        Me.TxtCargo.Location = New System.Drawing.Point(345, 31)
+        Me.TxtCargo.Name = "TxtCargo"
+        Me.TxtCargo.Size = New System.Drawing.Size(138, 13)
+        Me.TxtCargo.TabIndex = 8
+        Me.TxtCargo.Text = "Cargo: Analista de Sistemas"
         '
-        'RegistroBindingSource
+        'TxtDpto
         '
-        Me.RegistroBindingSource.DataMember = "Registro"
-        Me.RegistroBindingSource.DataSource = Me.DataSet1BindingSource
+        Me.TxtDpto.AutoSize = True
+        Me.TxtDpto.Location = New System.Drawing.Point(517, 31)
+        Me.TxtDpto.Name = "TxtDpto"
+        Me.TxtDpto.Size = New System.Drawing.Size(127, 13)
+        Me.TxtDpto.TabIndex = 9
+        Me.TxtDpto.Text = "Departamento: Desarrollo"
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None
+        Me.GroupBox1.Location = New System.Drawing.Point(0, 57)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(685, 403)
+        Me.GroupBox1.TabIndex = 10
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "GroupBox1"
         '
         'FrmRegistros
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(686, 390)
-        Me.Controls.Add(Me.Label3)
-        Me.Controls.Add(Me.Label2)
-        Me.Controls.Add(Me.Label1)
+        Me.ClientSize = New System.Drawing.Size(686, 460)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.TxtDpto)
+        Me.Controls.Add(Me.TxtCargo)
+        Me.Controls.Add(Me.TxtEmpleado)
+        Me.Controls.Add(Me.TxtID)
+        Me.Controls.Add(Me.CbxBuscar)
         Me.Controls.Add(Me.ToolStrip1)
         Me.Name = "FrmRegistros"
         Me.Text = "FrmRegistros"
         Me.ToolStrip1.ResumeLayout(False)
         Me.ToolStrip1.PerformLayout()
-        CType(Me.RegistroBindingSource, System.ComponentModel.ISupportInitialize).EndInit()
-        CType(Me.DataSet1BindingSource, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
     End Sub
 
     Friend WithEvents ToolStrip1 As ToolStrip
-    Friend WithEvents ToolStripButton1 As ToolStripButton
+    Friend WithEvents BtnJustificar As ToolStripButton
     Friend WithEvents ToolStripSeparator3 As ToolStripSeparator
-    Friend WithEvents ToolStripButton4 As ToolStripButton
-    Friend WithEvents ToolStripTextBox1 As ToolStripTextBox
-    Friend WithEvents Label1 As Label
-    Friend WithEvents Label2 As Label
-    Friend WithEvents Label3 As Label
-    Friend WithEvents DataSet1BindingSource As BindingSource
-    Friend WithEvents RegistroBindingSource As BindingSource
+    Friend WithEvents BtnBuscar As ToolStripButton
+    Friend WithEvents CbxBuscar As ComboBox
+    Friend WithEvents TxtID As Label
+    Friend WithEvents TxtEmpleado As Label
+    Friend WithEvents TxtCargo As Label
+    Friend WithEvents TxtDpto As Label
+    Friend WithEvents GroupBox1 As GroupBox
 End Class
