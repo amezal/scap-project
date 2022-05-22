b65e8af21515ad9023e78dbb97a1d4878bbc10ed
Blanca Rosales <rosales.taizx@gmail.com>
2022-05-22 11:49:19 -0600

    añadir departamento agregado

--- Departamentos/FrmAddDpto.vb
+++ Departamentos/FrmAddDpto.vb
@@ -1,9 +1,21 @@
 ﻿Public Class FrmAddDpto
-    Private Sub Label1_Click(sender As Object, e As EventArgs) 
 
+    Dim dp As New LMBADataSetTableAdapters.DepartamentoTableAdapter
+    Private Sub btnGuardar_Click(sender As Object, e As EventArgs) Handles btnGuardar.Click
+        Try
+
+            Dim newDp As String = tbDepto.Text.Trim
+            Dim email As String = tbEmail.Text.Trim
+            Dim ext As Int32 = tbExt.Text.Trim
+            dp.InsertarDpto(newDp, ext, email)
+
+
+        Catch ex As Exception
+
+        End Try
     End Sub
 
-    Private Sub ToolStripLabel1_Click(sender As Object, e As EventArgs) 
-
+    Private Sub btnCancel_Click(sender As Object, e As EventArgs) Handles btnCancel.Click
+        Me.Close()
     End Sub
 End Class
