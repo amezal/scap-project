41e7e0e1fb7e28b5949b77f6f91431d21157305a
Armando <amezal01@hotmail.com>
2022-05-22 22:18:01 -0600

    interfaz temporal

--- Main/Form1.designer.vb
+++ Main/Form1.designer.vb
@@ -26,64 +26,100 @@ Partial Class Form1
         Me.Button1 = New System.Windows.Forms.Button()
         Me.Button2 = New System.Windows.Forms.Button()
         Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Button6 = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
         'Label1
         '
-        Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(191, 53)
+        Me.Label1.BackColor = System.Drawing.Color.Linen
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 24.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(11, 9)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(124, 20)
+        Me.Label1.Size = New System.Drawing.Size(425, 148)
         Me.Label1.TabIndex = 0
-        Me.Label1.Text = "ACCEDER COMO:"
+        Me.Label1.Text = "INTERFAZ TEMPORAL MIENTRAS LUIS HACE SU PARTE"
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
         '
         'Button1
         '
-        Me.Button1.BackColor = System.Drawing.SystemColors.ControlText
-        Me.Button1.ForeColor = System.Drawing.Color.Cornsilk
-        Me.Button1.Location = New System.Drawing.Point(180, 144)
+        Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!)
+        Me.Button1.Location = New System.Drawing.Point(90, 184)
         Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(147, 43)
+        Me.Button1.Size = New System.Drawing.Size(238, 37)
         Me.Button1.TabIndex = 1
-        Me.Button1.Text = "EMPLEADO"
-        Me.Button1.UseVisualStyleBackColor = False
+        Me.Button1.Text = "Usuarios"
+        Me.Button1.UseVisualStyleBackColor = True
         '
         'Button2
         '
-        Me.Button2.BackColor = System.Drawing.SystemColors.ControlText
-        Me.Button2.ForeColor = System.Drawing.Color.Cornsilk
-        Me.Button2.Location = New System.Drawing.Point(180, 253)
+        Me.Button2.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!)
+        Me.Button2.Location = New System.Drawing.Point(90, 231)
         Me.Button2.Name = "Button2"
-        Me.Button2.Size = New System.Drawing.Size(147, 43)
+        Me.Button2.Size = New System.Drawing.Size(238, 37)
         Me.Button2.TabIndex = 2
-        Me.Button2.Text = "ADMINISTRADOR"
-        Me.Button2.UseVisualStyleBackColor = False
+        Me.Button2.Text = "Horarios"
+        Me.Button2.UseVisualStyleBackColor = True
         '
         'Button3
         '
-        Me.Button3.BackColor = System.Drawing.SystemColors.ControlText
-        Me.Button3.ForeColor = System.Drawing.Color.Cornsilk
-        Me.Button3.Location = New System.Drawing.Point(12, 367)
+        Me.Button3.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!)
+        Me.Button3.Location = New System.Drawing.Point(90, 278)
         Me.Button3.Name = "Button3"
-        Me.Button3.Size = New System.Drawing.Size(147, 34)
+        Me.Button3.Size = New System.Drawing.Size(238, 37)
         Me.Button3.TabIndex = 3
-        Me.Button3.Text = "SALIR"
-        Me.Button3.UseVisualStyleBackColor = False
+        Me.Button3.Text = "Añadir Departamento"
+        Me.Button3.UseVisualStyleBackColor = True
         '
-        'FrmAccesoPrincipal
+        'Button4
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 20.0!)
+        Me.Button4.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!)
+        Me.Button4.Location = New System.Drawing.Point(90, 323)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(238, 37)
+        Me.Button4.TabIndex = 4
+        Me.Button4.Text = "Cargos"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Button5
+        '
+        Me.Button5.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!)
+        Me.Button5.Location = New System.Drawing.Point(90, 369)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(238, 37)
+        Me.Button5.TabIndex = 5
+        Me.Button5.Text = "Empleados"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'Button6
+        '
+        Me.Button6.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!)
+        Me.Button6.Location = New System.Drawing.Point(90, 413)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(238, 37)
+        Me.Button6.TabIndex = 6
+        Me.Button6.Text = "Registros"
+        Me.Button6.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(509, 413)
+        Me.ClientSize = New System.Drawing.Size(447, 510)
+        Me.Controls.Add(Me.Button6)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Button4)
         Me.Controls.Add(Me.Button3)
         Me.Controls.Add(Me.Button2)
         Me.Controls.Add(Me.Button1)
         Me.Controls.Add(Me.Label1)
-        Me.Name = "FrmAccesoPrincipal"
+        Me.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.Name = "Form1"
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "FrmAccesoPrincipal"
         Me.ResumeLayout(False)
-        Me.PerformLayout()
 
     End Sub
 
@@ -91,4 +127,7 @@ Partial Class Form1
     Friend WithEvents Button1 As Button
     Friend WithEvents Button2 As Button
     Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Button6 As Button
 End Class
--- Main/Form1.vb
+++ Main/Form1.vb
@@ -7,11 +7,35 @@
 
     End Sub
 
-    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+    Private Sub Button1_Click(sender As Object, e As EventArgs)
 
     End Sub
 
     Private Sub FrmAccesoPrincipal_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
+    End Sub
+
+    Private Sub Button1_Click_1(sender As Object, e As EventArgs) Handles Button1.Click
+        FrmPwd.Show()
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        FrmHorarios.Show()
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        FrmAddDpto.Show()
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        FrmCargo.Show()
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        FrmEmpleados.Show()
+    End Sub
+
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+        FrmRegistros.Show()
     End Sub
 End Class
