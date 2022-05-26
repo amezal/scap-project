0cd3265db9ba26705cdc074a77b440b5642c29a5
Mabel Garcia <hmabel4922@gmail.com>
2022-05-26 14:18:04 -0600

    frmUsuario actualizado

--- adminPwd/FrmAddPwd.designer.vb
+++ adminPwd/FrmAddPwd.designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class FrmAddPwd
     Inherits System.Windows.Forms.Form
 
     'Form reemplaza a Dispose para limpiar la lista de componentes.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,17 +20,31 @@ Partial Class FrmAddPwd
     'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
     'Se puede modificar usando el Diseñador de Windows Forms.  
     'No lo modifique con el editor de código.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(FrmAddPwd))
         Me.ToolStrip1 = New System.Windows.Forms.ToolStrip()
         Me.btnSave = New System.Windows.Forms.ToolStripButton()
         Me.ToolStripSeparator1 = New System.Windows.Forms.ToolStripSeparator()
         Me.btnCancel = New System.Windows.Forms.ToolStripButton()
-        Me.cbxUsername = New System.Windows.Forms.ComboBox()
-        Me.Label1 = New System.Windows.Forms.Label()
-        Me.txtPwd = New System.Windows.Forms.TextBox()
+        Me.txtID = New System.Windows.Forms.TextBox()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.TxtEmail = New System.Windows.Forms.TextBox()
         Me.Label2 = New System.Windows.Forms.Label()
+        Me.TxtApellido = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TxtUser = New System.Windows.Forms.TextBox()
+        Me.TxtNombre = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label0 = New System.Windows.Forms.Label()
+        Me.BtnPw2 = New System.Windows.Forms.Button()
+        Me.BtnPwd = New System.Windows.Forms.Button()
+        Me.TxtPwd2 = New System.Windows.Forms.TextBox()
+        Me.TxtPwd = New System.Windows.Forms.TextBox()
+        Me.Label16 = New System.Windows.Forms.Label()
+        Me.Label15 = New System.Windows.Forms.Label()
+        Me.ChkEstado = New System.Windows.Forms.CheckBox()
+        Me.Label17 = New System.Windows.Forms.Label()
         Me.ToolStrip1.SuspendLayout()
         Me.SuspendLayout()
         '
@@ -67,48 +81,206 @@ Partial Class FrmAddPwd
         Me.btnCancel.Text = "Cancelar"
         Me.btnCancel.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage
         '
-        'cbxUsername
+        'txtID
         '
-        Me.cbxUsername.FormattingEnabled = True
-        Me.cbxUsername.Location = New System.Drawing.Point(280, 125)
-        Me.cbxUsername.Name = "cbxUsername"
-        Me.cbxUsername.Size = New System.Drawing.Size(371, 24)
-        Me.cbxUsername.TabIndex = 22
+        Me.txtID.Enabled = False
+        Me.txtID.Location = New System.Drawing.Point(402, 59)
+        Me.txtID.Margin = New System.Windows.Forms.Padding(4)
+        Me.txtID.Name = "txtID"
+        Me.txtID.Size = New System.Drawing.Size(216, 22)
+        Me.txtID.TabIndex = 95
         '
-        'Label1
+        'Label8
         '
-        Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(136, 125)
-        Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(38, 17)
-        Me.Label1.TabIndex = 21
-        Me.Label1.Text = "User"
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(318, 61)
+        Me.Label8.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(25, 17)
+        Me.Label8.TabIndex = 99
+        Me.Label8.Text = "ID:"
         '
-        'txtPwd
+        'TxtEmail
         '
-        Me.txtPwd.Location = New System.Drawing.Point(280, 184)
-        Me.txtPwd.Name = "txtPwd"
-        Me.txtPwd.Size = New System.Drawing.Size(176, 22)
-        Me.txtPwd.TabIndex = 24
+        Me.TxtEmail.Location = New System.Drawing.Point(402, 180)
+        Me.TxtEmail.Margin = New System.Windows.Forms.Padding(4)
+        Me.TxtEmail.Name = "TxtEmail"
+        Me.TxtEmail.Size = New System.Drawing.Size(216, 22)
+        Me.TxtEmail.TabIndex = 88
         '
         'Label2
         '
         Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(105, 187)
+        Me.Label2.Location = New System.Drawing.Point(297, 184)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(69, 17)
-        Me.Label2.TabIndex = 23
-        Me.Label2.Text = "Password"
+        Me.Label2.Size = New System.Drawing.Size(46, 17)
+        Me.Label2.TabIndex = 98
+        Me.Label2.Text = "Email:"
+        Me.Label2.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        '
+        'TxtApellido
+        '
+        Me.TxtApellido.Location = New System.Drawing.Point(402, 132)
+        Me.TxtApellido.Margin = New System.Windows.Forms.Padding(4)
+        Me.TxtApellido.Name = "TxtApellido"
+        Me.TxtApellido.Size = New System.Drawing.Size(216, 22)
+        Me.TxtApellido.TabIndex = 91
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(281, 139)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(62, 17)
+        Me.Label1.TabIndex = 96
+        Me.Label1.Text = "Apellido:"
+        '
+        'TxtUser
+        '
+        Me.TxtUser.Location = New System.Drawing.Point(402, 224)
+        Me.TxtUser.Margin = New System.Windows.Forms.Padding(4)
+        Me.TxtUser.Name = "TxtUser"
+        Me.TxtUser.Size = New System.Drawing.Size(216, 22)
+        Me.TxtUser.TabIndex = 90
+        '
+        'TxtNombre
+        '
+        Me.TxtNombre.Location = New System.Drawing.Point(402, 95)
+        Me.TxtNombre.Margin = New System.Windows.Forms.Padding(4)
+        Me.TxtNombre.Name = "TxtNombre"
+        Me.TxtNombre.Size = New System.Drawing.Size(216, 22)
+        Me.TxtNombre.TabIndex = 89
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(301, 228)
+        Me.Label3.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(42, 17)
+        Me.Label3.TabIndex = 94
+        Me.Label3.Text = "User:"
+        Me.Label3.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        '
+        'Label0
+        '
+        Me.Label0.AutoSize = True
+        Me.Label0.Location = New System.Drawing.Point(281, 100)
+        Me.Label0.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label0.Name = "Label0"
+        Me.Label0.Size = New System.Drawing.Size(62, 17)
+        Me.Label0.TabIndex = 93
+        Me.Label0.Text = "Nombre:"
+        Me.Label0.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        '
+        'BtnPw2
+        '
+        Me.BtnPw2.Location = New System.Drawing.Point(534, 309)
+        Me.BtnPw2.Margin = New System.Windows.Forms.Padding(4)
+        Me.BtnPw2.Name = "BtnPw2"
+        Me.BtnPw2.Size = New System.Drawing.Size(85, 25)
+        Me.BtnPw2.TabIndex = 103
+        Me.BtnPw2.Text = "Ver"
+        Me.BtnPw2.UseVisualStyleBackColor = True
+        '
+        'BtnPwd
+        '
+        Me.BtnPwd.Location = New System.Drawing.Point(534, 265)
+        Me.BtnPwd.Margin = New System.Windows.Forms.Padding(4)
+        Me.BtnPwd.Name = "BtnPwd"
+        Me.BtnPwd.Size = New System.Drawing.Size(85, 25)
+        Me.BtnPwd.TabIndex = 101
+        Me.BtnPwd.Text = "Ver"
+        Me.BtnPwd.UseVisualStyleBackColor = True
+        '
+        'TxtPwd2
+        '
+        Me.TxtPwd2.Location = New System.Drawing.Point(402, 309)
+        Me.TxtPwd2.Margin = New System.Windows.Forms.Padding(4)
+        Me.TxtPwd2.MaxLength = 4
+        Me.TxtPwd2.Name = "TxtPwd2"
+        Me.TxtPwd2.PasswordChar = Global.Microsoft.VisualBasic.ChrW(42)
+        Me.TxtPwd2.Size = New System.Drawing.Size(123, 22)
+        Me.TxtPwd2.TabIndex = 102
+        '
+        'TxtPwd
+        '
+        Me.TxtPwd.Location = New System.Drawing.Point(402, 265)
+        Me.TxtPwd.Margin = New System.Windows.Forms.Padding(4)
+        Me.TxtPwd.MaxLength = 4
+        Me.TxtPwd.Name = "TxtPwd"
+        Me.TxtPwd.PasswordChar = Global.Microsoft.VisualBasic.ChrW(42)
+        Me.TxtPwd.Size = New System.Drawing.Size(123, 22)
+        Me.TxtPwd.TabIndex = 100
+        '
+        'Label16
+        '
+        Me.Label16.AutoSize = True
+        Me.Label16.Location = New System.Drawing.Point(195, 310)
+        Me.Label16.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label16.Name = "Label16"
+        Me.Label16.Size = New System.Drawing.Size(148, 17)
+        Me.Label16.TabIndex = 105
+        Me.Label16.Text = "Confirmar contraseña:"
+        Me.Label16.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        '
+        'Label15
+        '
+        Me.Label15.AutoSize = True
+        Me.Label15.Location = New System.Drawing.Point(258, 270)
+        Me.Label15.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label15.Name = "Label15"
+        Me.Label15.Size = New System.Drawing.Size(85, 17)
+        Me.Label15.TabIndex = 104
+        Me.Label15.Text = "Contraseña:"
+        Me.Label15.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        '
+        'ChkEstado
+        '
+        Me.ChkEstado.AutoSize = True
+        Me.ChkEstado.Location = New System.Drawing.Point(402, 346)
+        Me.ChkEstado.Margin = New System.Windows.Forms.Padding(4)
+        Me.ChkEstado.Name = "ChkEstado"
+        Me.ChkEstado.Size = New System.Drawing.Size(18, 17)
+        Me.ChkEstado.TabIndex = 106
+        Me.ChkEstado.UseVisualStyleBackColor = True
+        '
+        'Label17
+        '
+        Me.Label17.AutoSize = True
+        Me.Label17.Location = New System.Drawing.Point(283, 345)
+        Me.Label17.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label17.Name = "Label17"
+        Me.Label17.Size = New System.Drawing.Size(60, 17)
+        Me.Label17.TabIndex = 107
+        Me.Label17.Text = "Estado: "
+        Me.Label17.TextAlign = System.Drawing.ContentAlignment.MiddleRight
         '
         'FrmAddPwd
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Controls.Add(Me.txtPwd)
+        Me.Controls.Add(Me.ChkEstado)
+        Me.Controls.Add(Me.Label17)
+        Me.Controls.Add(Me.BtnPw2)
+        Me.Controls.Add(Me.BtnPwd)
+        Me.Controls.Add(Me.TxtPwd2)
+        Me.Controls.Add(Me.TxtPwd)
+        Me.Controls.Add(Me.Label16)
+        Me.Controls.Add(Me.Label15)
+        Me.Controls.Add(Me.txtID)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.TxtEmail)
         Me.Controls.Add(Me.Label2)
-        Me.Controls.Add(Me.cbxUsername)
+        Me.Controls.Add(Me.TxtApellido)
         Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TxtUser)
+        Me.Controls.Add(Me.TxtNombre)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label0)
         Me.Controls.Add(Me.ToolStrip1)
         Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
         Me.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
@@ -124,8 +296,22 @@ Partial Class FrmAddPwd
     Friend WithEvents btnSave As ToolStripButton
     Friend WithEvents ToolStripSeparator1 As ToolStripSeparator
     Friend WithEvents btnCancel As ToolStripButton
-    Friend WithEvents cbxUsername As ComboBox
-    Friend WithEvents Label1 As Label
-    Friend WithEvents txtPwd As TextBox
+    Friend WithEvents txtID As TextBox
+    Friend WithEvents Label8 As Label
+    Friend WithEvents TxtEmail As TextBox
     Friend WithEvents Label2 As Label
+    Friend WithEvents TxtApellido As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents TxtUser As TextBox
+    Friend WithEvents TxtNombre As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label0 As Label
+    Friend WithEvents BtnPw2 As Button
+    Friend WithEvents BtnPwd As Button
+    Friend WithEvents TxtPwd2 As TextBox
+    Friend WithEvents TxtPwd As TextBox
+    Friend WithEvents Label16 As Label
+    Friend WithEvents Label15 As Label
+    Friend WithEvents ChkEstado As CheckBox
+    Friend WithEvents Label17 As Label
 End Class
