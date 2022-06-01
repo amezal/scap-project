33d522e55ea654c2740f5ac03bdbc94abeafc3e5
Armando <amezal01@hotmail.com>
2022-06-01 10:07:41 -0600

    Login e interfaces

--- Main/FrmConfig.vb
+++ Main/FrmConfig.vb
@@ -1,3 +1,25 @@
 ﻿Public Class FrmConfig
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        FrmPwd.Show()
+    End Sub
 
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        FrmHorarios.Show()
+    End Sub
+
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+        FrmCargo.Show()
+    End Sub
+
+    Private Sub Button7_Click(sender As Object, e As EventArgs) Handles Button7.Click
+        FrmDpto.Show()
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        FrmRol.Show()
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        FrmOpcion.Show()
+    End Sub
 End Class
--- Main/FrmLoginEmpleado.vb
+++ Main/FrmLoginEmpleado.vb
@@ -1,25 +1,34 @@
 ﻿Public Class FrmLoginEmpleado
-    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+    Dim empleados As New LMBADataSetTableAdapters.EmpleadoTableAdapter
 
+    Private Sub FrmLoginAdmin_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim emp = empleados.GetData()
+        emp.Columns.Add("display", GetType(String), " primerNombre + ' ' + primerApellido + ' - ' + idEmpleado")
+        cbxEmp.DataSource = emp
+        cbxEmp.DisplayMember = "display"
+        cbxEmp.ValueMember = "idEmpleado"
     End Sub
 
-    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs)
+    Private Sub btnIngresar_Click(sender As Object, e As EventArgs) Handles btnIngresar.Click
+        Dim valido As Boolean
+        Try
+            valido = empleados.GetData.Select($"idEmpleado='{cbxEmp.SelectedValue}' AND PIN='{txtPIN.Text}'").Count > 0
+            If (valido) Then
+                MsgBox("Bienvenido!", MsgBoxStyle.MsgBoxHelp, "Éxito")
+                FrmMarcarES.idEmp = cbxEmp.SelectedValue
+                FrmMarcarES.Show()
+                Me.Hide()
+                txtPIN.Text = ""
+            Else
+                MsgBox("Inicio de sesión incorrecto", MsgBoxStyle.Critical, "Error")
+            End If
 
+        Catch ex As Exception
+            MsgBox(ex.Message, MsgBoxStyle.Critical, "Error")
+        End Try
     End Sub
 
-    Private Sub Button1_Click(sender As Object, e As EventArgs)
-
-    End Sub
-
-    Private Sub Button1_Click_1(sender As Object, e As EventArgs) Handles Button1.Click
-
-    End Sub
-
-    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
-
-    End Sub
-
-    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-
+    Private Sub btnSalir_Click(sender As Object, e As EventArgs) Handles btnSalir.Click
+        Application.Exit()
     End Sub
 End Class
