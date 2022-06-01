350d105ff85e511b56f907f82a5a912099321eb7
Armando <amezal01@hotmail.com>
2022-06-01 01:51:33 -0600

    roles

---
+++ Roles/FrmAddRol.Designer.vb
@@ -0,0 +1,83 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class FrmAddRol
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.ToolStrip1 = New System.Windows.Forms.ToolStrip()
+        Me.ToolStripSeparator1 = New System.Windows.Forms.ToolStripSeparator()
+        Me.BtnGuardar = New System.Windows.Forms.ToolStripButton()
+        Me.BtnCancelar = New System.Windows.Forms.ToolStripButton()
+        Me.ToolStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'ToolStrip1
+        '
+        Me.ToolStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.BtnGuardar, Me.ToolStripSeparator1, Me.BtnCancelar})
+        Me.ToolStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.ToolStrip1.Name = "ToolStrip1"
+        Me.ToolStrip1.Size = New System.Drawing.Size(614, 25)
+        Me.ToolStrip1.TabIndex = 30
+        Me.ToolStrip1.Text = "ToolStrip1"
+        '
+        'ToolStripSeparator1
+        '
+        Me.ToolStripSeparator1.Name = "ToolStripSeparator1"
+        Me.ToolStripSeparator1.Size = New System.Drawing.Size(6, 25)
+        '
+        'BtnGuardar
+        '
+        Me.BtnGuardar.Image = Global.SCAP_LMBA_Tech.My.Resources.Resources.icons8_save_26
+        Me.BtnGuardar.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.BtnGuardar.Name = "BtnGuardar"
+        Me.BtnGuardar.Size = New System.Drawing.Size(69, 22)
+        Me.BtnGuardar.Text = "Guardar"
+        Me.BtnGuardar.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage
+        '
+        'BtnCancelar
+        '
+        Me.BtnCancelar.Image = Global.SCAP_LMBA_Tech.My.Resources.Resources.icons8_cancel_50
+        Me.BtnCancelar.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.BtnCancelar.Name = "BtnCancelar"
+        Me.BtnCancelar.Size = New System.Drawing.Size(73, 22)
+        Me.BtnCancelar.Text = "Cancelar"
+        Me.BtnCancelar.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage
+        '
+        'FrmAddRol
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(614, 450)
+        Me.Controls.Add(Me.ToolStrip1)
+        Me.Name = "FrmAddRol"
+        Me.Text = "FrmAddRol"
+        Me.ToolStrip1.ResumeLayout(False)
+        Me.ToolStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents ToolStrip1 As ToolStrip
+    Friend WithEvents BtnGuardar As ToolStripButton
+    Friend WithEvents ToolStripSeparator1 As ToolStripSeparator
+    Friend WithEvents BtnCancelar As ToolStripButton
+End Class
---
+++ Roles/FrmRol.vb
@@ -0,0 +1,42 @@
+﻿Public Class FrmRol
+    Dim query As String = "%"
+    Dim rolActual As Integer
+    Dim roldgv As New LMBADataSetTableAdapters.tbl_rolDgv
+
+    Private Sub FrmRol_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ListarRoles()
+        BtnMod.Enabled = False
+        BtnEliminar.Enabled = False
+    End Sub
+
+    Private Sub ListarRoles()
+        DgvRoles.DataSource = roldgv.GetData(query)
+        DgvRoles.Columns(0).Visible = False
+        DgvRoles.Columns(1).HeaderText = "Rol"
+        DgvRoles.Columns(2).HeaderText = "Opciones"
+        DgvRoles.Columns(2).AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill
+    End Sub
+
+    Public Sub Actualizar()
+        DgvRoles.DataSource = roldgv.GetData(query)
+    End Sub
+
+    Private Sub TxtBuscar_TextChanged(sender As Object, e As EventArgs) Handles TxtBuscar.TextChanged
+        query = TxtBuscar.Text + "%"
+        Actualizar()
+    End Sub
+
+    Private Sub DgvRoles_CellDoubleClick(sender As Object, e As DataGridViewCellEventArgs) Handles DgvRoles.CellDoubleClick
+        Dim row = DgvRoles.CurrentRow.Index
+        Dim rol = DgvRoles.Rows(row)
+        Dim id = rol.Cells(0).Value
+        rolActual = id
+        BtnMod.Enabled = True
+        BtnEliminar.Enabled = True
+    End Sub
+
+    Private Sub BtnMod_Click(sender As Object, e As EventArgs) Handles BtnMod.Click
+        FrmModRol.idRol = rolActual
+        FrmModRol.Show()
+    End Sub
+End Class
