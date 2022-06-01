671683fc8a31ced4ddcd35ac8c98f3a085233a26
MrLuisJ <luisjimenezrizo@gmail.com>
2022-06-01 12:16:20 -0600

    ¨reportes¨

---
+++ Reportes/VwDepartamento.vb
@@ -0,0 +1,6 @@
+﻿Public Class VwDepartamento
+    Private Sub VwDepartamento_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        Me.ReportViewer1.RefreshReport()
+    End Sub
+End Class
